98cc5543ff6c3c2d843316520fd62afdc72bea6a
pihanhsu <pihanhsu@gmail.com>
2018-11-30 22:30:52 +0800

    update image size and orderlist view

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,5 +1,5 @@
 class Restaurant < ApplicationRecord
-	has_attached_file :menu_photo, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
+	has_attached_file :menu_photo, styles: { medium: "600x600>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
   validates_attachment_content_type :menu_photo, content_type: /\Aimage\/.*\z/
 
   has_many :orders
