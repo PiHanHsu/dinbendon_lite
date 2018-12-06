fe4515f244d4e4179fdec635eefcfd1d206b6b84
pihanhsu <pihanhsu@gmail.com>
2018-12-07 00:12:18 +0800

    update mailer layout

--- app/mailers/order_confirm_mailer.rb
+++ app/mailers/order_confirm_mailer.rb
@@ -2,6 +2,7 @@ class OrderConfirmMailer < ApplicationMailer
 
 	def send_orderlist(user, order)
 		@user = user
+		@order = order
 		@orderlists = order.orderlists
     mail to:@user.email, subject:"訂購清單明細！"
 	end
